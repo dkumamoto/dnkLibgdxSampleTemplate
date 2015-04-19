6a223b22c8c2b48bda0032c6160ea638211e06e4
Danny Kumamoto <dkumamoto@users.noreply.github.com>
2015-04-18 18:52:09 -0700

    Initial set of files created by Libgdx project generator

---
+++ android/src/com/dnk/libgdx/sample/template/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dnk.libgdx.sample.template.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dnk.libgdx.sample.template.dnkLibgdxSampleTemplate;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new dnkLibgdxSampleTemplate(), config);
+	}
+}
---
+++ desktop/src/com/dnk/libgdx/sample/template/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dnk.libgdx.sample.template.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dnk.libgdx.sample.template.dnkLibgdxSampleTemplate;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new dnkLibgdxSampleTemplate(), config);
+	}
+}
